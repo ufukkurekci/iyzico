7fea311a713653aba477c7683937a2afb60122da
ufukkurekcii <ufukkurekci@hotmail.com>
2023-12-18 20:46:21 +0300

    sertifika kuruldu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
 import config from "./config";
-import express from "express"
-import logger from "morgan"
+import express from "express";
+import logger from "morgan";
+import https from "https";
+import fs from "fs";
+import path from "path";
 
 
 const envPath = config?.production
@@ -25,4 +28,22 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-app.listen(process.env.PORT, () => console.log("Express app start listen " + process.env.PORT))
+
+if(process.env.HTTPS_ENABLED === "true")
+{
+    const key = fs.readFileSync(path.join(__dirname,"./certs/key.pem")).toString();
+    const cert = fs.readFileSync(path.join(__dirname,"./certs/cert.pem")).toString();
+
+    const server = https.createServer({
+        key: key,
+        cert: cert
+    },app);
+
+    server.listen(process.env.PORT, () => console.log("Express app start listen " + process.env.PORT));
+}
+else{
+    app.listen(process.env.PORT, () => console.log("Express app start listen " + process.env.PORT))
+
+}
+
+
